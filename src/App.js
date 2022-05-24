089ceb7e624314437687aac95d51a8f041ac1f03
Jake Felice <jasonfelice0@gmail.com>
2022-05-24 07:31:45 -0700

    Set up the calculator ui template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Calculator</h1>
-    </div>
-  );
+  return <Calculator />;
 }
 
 export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <div className="output" />
+        <div className="btns func"> AC </div>
+        <div className="btns func"> +/- </div>
+        <div className="btns func"> % </div>
+        <div className="btns func"> ÷ </div>
+        <div className="btns"> 7 </div>
+        <div className="btns"> 8 </div>
+        <div className="btns"> 9 </div>
+        <div className="btns"> X </div>
+        <div className="btns"> 4 </div>
+        <div className="btns"> 5 </div>
+        <div className="btns"> 6 </div>
+        <div className="btns func"> - </div>
+        <div className="btns"> 1 </div>
+        <div className="btns"> 2 </div>
+        <div className="btns"> 3 </div>
+        <div className="btns func"> + </div>
+        <div className="btns"> 0 </div>
+        <div className="btns"> . </div>
+        <div className="btns func"> = </div>
+      </div>
+    );
+  }
+}
