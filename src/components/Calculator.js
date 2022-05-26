9d0a8a592113a7f2d59149ee898134671690949c
Jake Felice <jasonfelice0@gmail.com>
2022-05-26 08:09:56 -0700

    Pass button component as children inside the calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import calculate from '../logic/calculate';
+import Button from './Button';
 
 export default class Calculator extends Component {
   constructor(props) {
@@ -13,42 +14,30 @@ export default class Calculator extends Component {
     };
   }
 
-  componentDidMount() {
-    document.querySelector('.btns').addEventListener('click', this.work);
-  }
-
-  work(e) {
-    let vali = this.props;
-    vali = e.target.textContent;
-    setState(calculate({}, vali));
-  }
-
   render() {
     const { total } = this.state;
     return (
       <div className="calculator">
-        <div className="output">
-          {total}
-        </div>
-        <div className="btns func">AC</div>
-        <div className="btns func">+/-</div>
-        <div className="btns func">%</div>
-        <div className="btns func">÷</div>
-        <div className="btns">7</div>
-        <div className="btns">8</div>
-        <div className="btns">9</div>
-        <div className="btns func">x</div>
-        <div className="btns">4</div>
-        <div className="btns">5</div>
-        <div className="btns">6</div>
-        <div className="btns func">-</div>
-        <div className="btns">1</div>
-        <div className="btns">2</div>
-        <div className="btns">3</div>
-        <div className="btns func">+</div>
-        <div className="btns">0</div>
-        <div className="btns">.</div>
-        <div className="btns func">=</div>
+        <div className="output"></div>
+       <Button value="AC" btnClass="btns func" />
+       <Button value="+/-" btnClass="btns func"  />
+       <Button value="%" btnClass="btns func" />
+       <Button value="÷" btnClass="btns func" />
+       <Button value="7" />
+       <Button value="8" />
+       <Button value="9" />
+       <Button value="x" btnClass="btns func" />
+       <Button value="4" />
+       <Button value="5" />
+       <Button value="6" />
+       <Button value="-" btnClass="btns func" />
+       <Button value="1" />
+       <Button value="2" />
+       <Button value="3" />
+       <Button value="+" btnClass="btns func" />
+       <Button value="0" />
+       <Button value="." />
+       <Button value="=" btnClass="btns func" />
       </div>
     );
   }
