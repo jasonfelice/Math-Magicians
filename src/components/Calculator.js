d9c6c22ef006644354d16a4a24f5537b72dd3b6b
Jake Felice <jasonfelice0@gmail.com>
2022-05-26 08:51:29 -0700

    Enable linter check for Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 import React, { Component } from 'react';
 import calculate from '../logic/calculate';
 import Button from './Button';
@@ -17,33 +15,33 @@ export default class Calculator extends Component {
 
   eventHandler(value) {
     const { total, next, operation } = this.state;
-    this.setState(calculate({total, next, operation}, value));
+    this.setState(calculate({ total, next, operation }, value));
   }
 
   render() {
-    const {total, next, operation} = this.state; 
+    const { total, next, operation } = this.state;
     return (
       <div className="calculator">
         <div className="output">{total || next || operation}</div>
-       <Button handler={this.eventHandler} value="AC" btnClass="btns func" />
-       <Button handler={this.eventHandler} value="+/-" btnClass="btns func"  />
-       <Button handler={this.eventHandler} value="%" btnClass="btns func" />
-       <Button handler={this.eventHandler} value="÷" btnClass="btns func" />
-       <Button handler={this.eventHandler} value="7" />
-       <Button handler={this.eventHandler} value="8" />
-       <Button handler={this.eventHandler} value="9" />
-       <Button handler={this.eventHandler} value="x" btnClass="btns func" />
-       <Button handler={this.eventHandler} value="4" />
-       <Button handler={this.eventHandler} value="5" />
-       <Button handler={this.eventHandler} value="6" />
-       <Button handler={this.eventHandler} value="-" btnClass="btns func" />
-       <Button handler={this.eventHandler} value="1" />
-       <Button handler={this.eventHandler} value="2" />
-       <Button handler={this.eventHandler} value="3" />
-       <Button handler={this.eventHandler} value="+" btnClass="btns func" />
-       <Button handler={this.eventHandler} value="0" />
-       <Button handler={this.eventHandler} value="." />
-       <Button handler={this.eventHandler} value="=" btnClass="btns func" />
+        <Button handler={this.eventHandler} value="AC" btnClass="btns func" />
+        <Button handler={this.eventHandler} value="+/-" btnClass="btns func" />
+        <Button handler={this.eventHandler} value="%" btnClass="btns func" />
+        <Button handler={this.eventHandler} value="÷" btnClass="btns func" />
+        <Button handler={this.eventHandler} value="7" />
+        <Button handler={this.eventHandler} value="8" />
+        <Button handler={this.eventHandler} value="9" />
+        <Button handler={this.eventHandler} value="x" btnClass="btns func" />
+        <Button handler={this.eventHandler} value="4" />
+        <Button handler={this.eventHandler} value="5" />
+        <Button handler={this.eventHandler} value="6" />
+        <Button handler={this.eventHandler} value="-" btnClass="btns func" />
+        <Button handler={this.eventHandler} value="1" />
+        <Button handler={this.eventHandler} value="2" />
+        <Button handler={this.eventHandler} value="3" />
+        <Button handler={this.eventHandler} value="+" btnClass="btns func" />
+        <Button handler={this.eventHandler} value="0" />
+        <Button handler={this.eventHandler} value="." />
+        <Button handler={this.eventHandler} value="=" btnClass="btns func" />
       </div>
     );
   }
