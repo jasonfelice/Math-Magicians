b9bd0fc5c15e3d04402afbba74390dec496a5170
Jake Felice <jasonfelice0@gmail.com>
2022-05-27 04:13:04 -0700

    Refactor the Calculator component to a functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,48 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import Button from './Button';
 
-export default class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '0',
-      next: null,
-      operation: null,
-    };
-    this.eventHandler = this.eventHandler.bind(this);
-  }
+const Calculator = () => {
+  const [result, setResult] = useState({
+    total: '0',
+    next: null,
+    operation: null,
+  });
 
-  eventHandler(value) {
-    const { total, next, operation } = this.state;
-    this.setState(calculate({ total, next, operation }, value));
-  }
+  const eventHandler = (value) => {
+    const { total, next, operation } = result;
+    setResult(calculate({ total, next, operation }, value));
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calculator">
-        <div className="output">{total || next || operation}</div>
-        <Button handler={this.eventHandler} value="AC" btnClass="btns func" />
-        <Button handler={this.eventHandler} value="+/-" btnClass="btns func" />
-        <Button handler={this.eventHandler} value="%" btnClass="btns func" />
-        <Button handler={this.eventHandler} value="÷" btnClass="btns func" />
-        <Button handler={this.eventHandler} value="7" />
-        <Button handler={this.eventHandler} value="8" />
-        <Button handler={this.eventHandler} value="9" />
-        <Button handler={this.eventHandler} value="x" btnClass="btns func" />
-        <Button handler={this.eventHandler} value="4" />
-        <Button handler={this.eventHandler} value="5" />
-        <Button handler={this.eventHandler} value="6" />
-        <Button handler={this.eventHandler} value="-" btnClass="btns func" />
-        <Button handler={this.eventHandler} value="1" />
-        <Button handler={this.eventHandler} value="2" />
-        <Button handler={this.eventHandler} value="3" />
-        <Button handler={this.eventHandler} value="+" btnClass="btns func" />
-        <Button handler={this.eventHandler} value="0" />
-        <Button handler={this.eventHandler} value="." />
-        <Button handler={this.eventHandler} value="=" btnClass="btns func" />
-      </div>
-    );
-  }
-}
+  const { total, next, operation } = result;
+  return (
+    <div className="calculator">
+      <div className="output">{total || next || operation}</div>
+      <Button handler={eventHandler} value="AC" btnClass="btns func" />
+      <Button handler={eventHandler} value="+/-" btnClass="btns func" />
+      <Button handler={eventHandler} value="%" btnClass="btns func" />
+      <Button handler={eventHandler} value="÷" btnClass="btns func" />
+      <Button handler={eventHandler} value="7" />
+      <Button handler={eventHandler} value="8" />
+      <Button handler={eventHandler} value="9" />
+      <Button handler={eventHandler} value="x" btnClass="btns func" />
+      <Button handler={eventHandler} value="4" />
+      <Button handler={eventHandler} value="5" />
+      <Button handler={eventHandler} value="6" />
+      <Button handler={eventHandler} value="-" btnClass="btns func" />
+      <Button handler={eventHandler} value="1" />
+      <Button handler={eventHandler} value="2" />
+      <Button handler={eventHandler} value="3" />
+      <Button handler={eventHandler} value="+" btnClass="btns func" />
+      <Button handler={eventHandler} value="0" />
+      <Button handler={eventHandler} value="." />
+      <Button handler={eventHandler} value="=" btnClass="btns func" />
+    </div>
+  );
+};
+
+export default Calculator;
