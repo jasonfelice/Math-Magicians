073d138c40dfc5c21b0201b64d396c2ac76359c0
Jake Felice <jasonfelice0@gmail.com>
2022-06-01 03:08:52 -0700

    Style the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,20 @@ const Home = () => {
   const homeData = {
     title: 'Welcome!',
     des: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, distinctio. Quae eos fugiat corrupti ex fugit, repudiandae deleniti illo quisquam, rem veritatis cupiditate maiores itaque eligendi possimus quaerat quis expedita!',
+    des2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur adipisci est praesentium mollitia optio, iure, placeat esse odio consequatur minima in odit? Modi exercitationem aperiam laborum eaque, explicabo ut possimus?',
   };
 
-  const { title, des } = homeData;
+  const { title, des, des2 } = homeData;
 
   return (
     <>
       <Header />
       <section className="home">
         <h1>{title}</h1>
-        <p>{des}</p>
+        <div className="home-wrapper">
+          <p>{des}</p>
+          <p>{des2}</p>
+        </div>
       </section>
     </>
   );
