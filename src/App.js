aa0cde9f5556860dcddc38c6123e044aeb8c64a7
Jake Felice <jasonfelice0@gmail.com>
2022-06-01 03:44:15 -0700

    Finish the wireframe design of the webpage

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import {
 } from 'react-router-dom';
 import Home from './pages/Home';
 import Calculator from './pages/Calculator';
+import NoPage from './pages/NoPage';
+import Quote from './pages/Quote';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/calculator" element={<Calculator />} />
+      <Route path="*" element={<NoPage />} />
+      <Route path="/quote" element={<Quote />} />
     </Routes>
   </Router>
 );
